65c046fee145a69ada27de96b767bfb5da008d58
Salva <Salva@SALVA-PC>
2026-04-01 10:36:22 -0300

    Generamos Capeta de repositorios, y genreramos RepositorioGenerico para trabajar solamente ahi

---
+++ MyStore/Repositorios/RepositorioGenerico.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyStore.Contexto;
+
+namespace MyStore.Repositorios
+{
+    public class RepositorioGenerico<TEntidad>(AppDbContext _dbContext) where TEntidad : class
+    {
+        public async Task<IEnumerable<TEntidad>> GetAllAsync()
+        {
+            return await _dbContext.Set<TEntidad>().ToListAsync();
+
+        }
+    }
+}
