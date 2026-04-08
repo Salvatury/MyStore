ff19785fedac90fab81d469bce365752141d6608
Salva <Salva@SALVA-PC>
2026-04-08 00:43:45 -0300

    Agrego base de servicio, repositorio y modelo para productos

--- MyStore/Servicios/ProductoServicio.cs
+++ MyStore/Servicios/ProductoServicio.cs
@@ -2,6 +2,8 @@
 using MyStore.Models;
 using MyStore.Repositorios;
 using System.Linq.Expressions;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.IdentityModel.Tokens;
 
 namespace MyStore.Servicios
 {
@@ -13,33 +15,141 @@ namespace MyStore.Servicios
 
         public async Task<IEnumerable<ProductoVM>> TraerTodosAsync()
         {
-           var productos = await _repositorioProducto.TraerTodosAsync(
-               incluidos: new Expression<Func<Producto, object>>[] {x=> x.Categoria}            
-               
-               );
+            var productos = await _repositorioProducto.TraerTodosAsync(
+                incluidos: new Expression<Func<Producto, object>>[] { x => x.Categoria! }
+
+                );
+
             var productoVM = productos.Select(item =>
                 new ProductoVM
                 {
-                    ProductoId= item.ProductoId,
+                    ProductoId = item.ProductoId,
                     Categoria = new CategoriaVM
                     {
-                        CategoriaId = item.CategoriaId,
-                        Nombre = item.Categoria.Nombre
+                        CategoriaId = item.Categoria!.CategoriaId,
+                        Nombre = item.Categoria!.Nombre
                     },
                     Nombre = item.Nombre,
                     Descripcion = item.Descripcion,
                     Precio = item.Precio,
                     Stock = item.Stock,
                     NombreImagen = item.NombreImagen,
-
-
                 }).ToList();
 
             return productoVM;
 
+        }//ok
 
+        public async Task<ProductoVM> TraerPorIdAsync(int id)
+        {
+            var producto = await _repositorioProducto.TraerPorIdAsync(id);
+            var categorias = await _repositorioCategoria.TraerTodosAsync();
+
+            var productoVM = new ProductoVM();
+
+            if (producto != null)
+            {
+                productoVM = new ProductoVM
+                {
+                    ProductoId = producto.ProductoId,
+                    Categoria = new CategoriaVM
+                    {
+                        CategoriaId = producto.Categoria!.CategoriaId,
+                        Nombre = producto.Categoria!.Nombre,
+                    },
+                    Nombre = producto.Nombre,
+                    Descripcion = producto.Descripcion,
+                    Precio = producto.Precio,
+                    Stock = producto.Stock,
+                    NombreImagen = producto.NombreImagen,
+                };
+
+            }
+            productoVM.Categorias = categorias.Select(item => new SelectListItem
+            {
+                Value = item.CategoriaId.ToString(),
+                Text = item.Nombre
+            }).ToList();
+
+            return productoVM;
+
+
+        }//revisar
+
+
+
+        public async Task AgregarAsync(ProductoVM viewModel)
+        {
+            if (viewModel.ArchivoImagen != null)
+            {
+                string uploadFolder = Path.Combine(_webHostEnvironment.WebRootPath, "imagenes");
+                string uniqueFileName = Guid.NewGuid().ToString() + Path.GetExtension(viewModel.ArchivoImagen.FileName);
+                string filePath = Path.Combine(uploadFolder, uniqueFileName);
+                
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                    await viewModel.ArchivoImagen.CopyToAsync(stream);
+
+                viewModel.NombreImagen = uniqueFileName;
+            }
+            var entidad = new Producto
+            {
+                CategoriaId = viewModel.Categoria.CategoriaId,
+                Nombre = viewModel.Nombre,
+                Descripcion = viewModel.Descripcion,
+                Precio = viewModel.Precio,
+                Stock = viewModel.Stock,
+                NombreImagen = viewModel.NombreImagen
+            };
+
+            await _repositorioProducto.AgregarAsync(entidad);
+        }//revisar
+
+        public async Task EditarAsync(ProductoVM viewModel)
+        {
+            var producto = await _repositorioProducto.TraerPorIdAsync(viewModel.ProductoId);
+
+            if (viewModel.ArchivoImagen != null)
+            {
+                string uploadFolder = Path.Combine(_webHostEnvironment.WebRootPath, "imagenes");
+                string uniqueFileName = Guid.NewGuid().ToString() + Path.GetExtension(viewModel.ArchivoImagen.FileName);
+                string filePath = Path.Combine(uploadFolder, uniqueFileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                    await viewModel.ArchivoImagen.CopyToAsync(stream);
+
+
+                if (!producto.NombreImagen.IsNullOrEmpty())
+                {
+                    var imagenAnterior = producto.NombreImagen;
+                    string borrarRutaArchivo = Path.Combine(uploadFolder, imagenAnterior);
+
+                    if (File.Exists(borrarRutaArchivo)) File.Delete(borrarRutaArchivo);
+
+                }
+
+                viewModel.NombreImagen = uniqueFileName;
+            }
+            else
+            {
+                viewModel.NombreImagen = producto.NombreImagen;
+            }
+
+            producto.CategoriaId = viewModel.Categoria.CategoriaId;
+            producto.Nombre = viewModel.Nombre;
+            producto.Descripcion = viewModel.Descripcion;
+            producto.Precio = viewModel.Precio;
+            producto.Stock = viewModel.Stock;
+            producto.NombreImagen = viewModel.NombreImagen;
+
+            await _repositorioProducto.EditarAsync(producto);
 
 
         }
+
+        public async Task EliminarAsync(int id)
+        {
+            var producto = await _repositorioProducto.TraerPorIdAsync(id);
+            await _repositorioProducto.EliminarAsync(producto!);
+        }    
     }
 }
