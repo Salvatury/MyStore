6b069e2018acb5d5aa07eb242d87ba8dea3fc4b2
Salva <Salva@SALVA-PC>
2026-04-08 01:44:13 -0300

    Implemento CRUD de productos con imagen y categorias

---
+++ MyStore/Controllers/ProductoController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyStore.Models;
+using MyStore.Servicios;
+
+namespace MyStore.Controllers
+{
+    public class ProductoController(ProductoServicio _productoServicio) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var productos = await _productoServicio.TraerTodosAsync();
+            return View(productos);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AgregarEditar(int id)
+        {
+            var productoVM = await _productoServicio.TraerPorIdAsync(id);
+            return View(productoVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AgregarEditar(ProductoVM entidadVM)
+        {
+            ViewBag.Mensaje = null;
+
+            if (!ModelState.IsValid) return View(entidadVM);
+
+            if (entidadVM.ProductoId == 0)
+            {
+                await _productoServicio.AgregarAsync(entidadVM);
+                ModelState.Clear();
+                entidadVM = await _productoServicio.TraerPorIdAsync(0);
+                ViewBag.Mensaje = "Producto agregado correctamente";
+            }
+            else
+            {
+                await _productoServicio.EditarAsync(entidadVM);
+                ViewBag.Mensaje = "Producto editado correctamente";
+            }
+
+            return View(entidadVM);
+        }
+
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            await _productoServicio.EliminarAsync(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- MyStore/Program.cs
+++ MyStore/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddScoped(typeof(RepositorioGenerico<>));
 builder.Services.AddScoped<CategoriaServicio>();
+builder.Services.AddScoped<ProductoServicio>();
 
 var app = builder.Build();
 
